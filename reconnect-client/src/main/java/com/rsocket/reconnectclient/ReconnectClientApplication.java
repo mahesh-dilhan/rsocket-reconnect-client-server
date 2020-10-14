ca3008e708bc1ef2ac649b0f3161ccb8d62aeca9
mahesh <mail.mahesh.lk@gmail.com>
2020-10-14 17:03:32 +0800

    reconnect

--- reconnect-client/src/main/java/com/rsocket/reconnectclient/ReconnectClientApplication.java
+++ reconnect-client/src/main/java/com/rsocket/reconnectclient/ReconnectClientApplication.java
@@ -1,8 +1,22 @@
 package com.rsocket.reconnectclient;
 
+import lombok.extern.log4j.Log4j2;
+import org.reactivestreams.Publisher;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.MediaType;
+import org.springframework.messaging.rsocket.RSocketRequester;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import reactor.core.publisher.Mono;
+import reactor.util.retry.Retry;
 
+import java.awt.*;
+import java.time.Duration;
+
+@Log4j2
 @SpringBootApplication
 public class ReconnectClientApplication {
 
@@ -10,4 +24,35 @@ public class ReconnectClientApplication {
 		SpringApplication.run(ReconnectClientApplication.class, args);
 	}
 
+	@Bean
+	Mono<RSocketRequester> rSocketRequesterMono (RSocketRequester.Builder  builder){
+		return builder.rsocketConnector(
+				rSocketConnector -> rSocketConnector.reconnect(
+						Retry.fixedDelay(
+								Integer.MAX_VALUE, Duration.ofSeconds(1)
+						).doBeforeRetry(retrySignal -> log.info("Trying to ")
+				)
+		)
+		).connectTcp("192.168.1.75",7000);
+	}
+}
+
+@Log4j2
+@RestController
+class ReconnectClient {
+
+	@Autowired
+	private Mono<RSocketRequester>  rSocketRequesterMono;
+
+	@GetMapping(value = "/greet",produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+	public Publisher<String> greet(){
+		return rSocketRequesterMono.flatMapMany(
+				rSocketRequester ->
+						rSocketRequester
+								.route("reconnect-me")
+								.data(Mono.empty())
+								.retrieveFlux(String.class)
+								.doOnNext(s->log.info("{}",s))
+		);
+	}
 }
--- reconnect-server/src/main/java/com/rsocket/reconnectserver/ReconnectServerApplication.java
+++ reconnect-server/src/main/java/com/rsocket/reconnectserver/ReconnectServerApplication.java
@@ -2,6 +2,13 @@ package com.rsocket.reconnectserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.stereotype.Controller;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.stream.Stream;
 
 @SpringBootApplication
 public class ReconnectServerApplication {
@@ -10,4 +17,17 @@ public class ReconnectServerApplication {
 		SpringApplication.run(ReconnectServerApplication.class, args);
 	}
 
+}
+
+@Controller
+class ReconnectServer {
+
+	@MessageMapping("reconnect-me")
+	public Flux<String> greet(){
+		return Flux.fromStream(
+				Stream.generate(
+						() -> new String("Greet from Serevr" + Instant.now())
+				)
+		).delayElements(Duration.ofSeconds(1));
+	}
 }
